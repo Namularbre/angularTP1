62f0355ba6312804026923849e18e0bf5b1e5141
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-03-18 19:34:51 +0100

    ajout d'un formulaire pour le component

--- src/app/components/list-jobs/list-jobs.component.ts
+++ src/app/components/list-jobs/list-jobs.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms'
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-list-jobs',
@@ -7,7 +7,16 @@ import { FormBuilder } from '@angular/forms'
   styleUrls: ['./list-jobs.component.css']
 })
 export class ListJobsComponent {
+  //title of the component
   componentTitle : string = "Liste de jobs";
+
+  //reactive forms control
+  id = new FormControl('');
+  title = new FormControl('');
+  description = new FormControl('');
+  dateOfPublish = new FormControl(`${new Date()}`);
+  salary = new FormControl('');
+  location = new FormControl('');
 
   constructor(private formBuilder : FormBuilder) {}
 }
