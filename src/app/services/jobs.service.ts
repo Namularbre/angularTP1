22f1d449d1509f292653c3529e40fc14f7d7732d
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-03-18 18:53:46 +0100

    ajout d'un service chargé de retourner les offres d'emploi

---
+++ src/app/services/jobs.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Jobs } from '../models/jobs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobsService {
+  private readonly _jobs : Jobs[] = [
+    {
+      id : 1,
+      title : "Développeur C++/Qt",
+      description : "C pour une app tkt",
+      dateOfPublish : new Date(2023, 18, 3),
+      location : "Nantes",
+      salary : "35k"
+    },
+    {
+      id : 2,
+      title : "Développeur JavaScript",
+      description : "C pour une app tkt",
+      dateOfPublish : new Date(2023, 25, 3),
+      location : "Toulouse",
+      salary : "25k"
+    },
+    {
+      id : 3,
+      title : "Développeur TypeScript",
+      description : "C pour une app tkt",
+      dateOfPublish : new Date(2023, 4, 3),
+      location : "Limoges",
+      salary : "150k"
+    }
+  ];
+
+  constructor() { }
+
+  //of create an observable taking data from the array above
+  getJobs() : Observable<Jobs[]> {
+    return of(this._jobs);
+  }
+}
